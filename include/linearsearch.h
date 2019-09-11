6a5bca14cacd550e32f40de1e1464786b10614d7
Samuel IronSnake <TheIronSnake@yahoo.com>
2019-09-11 14:22:41 -0700

    linear search stoof

---
+++ include/linearsearch.h
@@ -0,0 +1,19 @@
+//
+// Created by vscilab on 9/11/2019.
+//
+
+#ifndef EX01_LINEARSEARC_LINEARSEARCH_H
+#define EX01_LINEARSEARC_LINEARSEARCH_H
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+
+                template <class T>
+                int64_t find(const T& value, T array[], size_t size) {
+                    if (array == nullptr) return -1;
+                    for (size_t i = 0; i < size; i++){
+                        if (value == array[i]) return i;
+                    }
+                    return -1;
+                }
+
+            }}}}
+#endif //EX01_LINEARSEARC_LINEARSEARCH_H
